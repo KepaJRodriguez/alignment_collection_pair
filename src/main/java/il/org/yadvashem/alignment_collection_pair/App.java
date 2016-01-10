4a033413b5b20a232387bda2d0a2b3f8ba6a0111
KepaJRodriguez <kepa.rodriguez@gmail.com>
2016-01-10 16:46:31 +0200

    Blank app file

---
+++ src/main/java/il/org/yadvashem/alignment_collection_pair/App.java
@@ -0,0 +1,18 @@
+package il.org.yadvashem.alignment_collection_pair;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        
+
+    	String ead_1 = args[0];
+    	String ead_2 = args[1];
+    	
+    	
+    }
+}
