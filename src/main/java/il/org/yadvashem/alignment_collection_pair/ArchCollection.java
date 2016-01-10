4941fb0ca06ab5f27da0bef97a6b153ed5c5f368
KepaJRodriguez <kepa.rodriguez@gmail.com>
2016-01-10 17:15:38 +0200

    Data structure to represent an archival collecion

---
+++ src/main/java/il/org/yadvashem/alignment_collection_pair/ArchCollection.java
@@ -0,0 +1,23 @@
+package il.org.yadvashem.alignment_collection_pair;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArchCollection {
+
+	// Provisional data structure. 
+	// Later in the system it will be improved with named entity extraction
+	// and topic recognition
+	
+	String TITLE_COLLECTION = "";
+	List<String> TITLES_RECORDS = new ArrayList<String>();
+	List<String> AP_PLACES = new ArrayList<String>();
+	List<String> AP_PERSON = new ArrayList<String>();
+	List<String> AP_ORGANIZATION = new ArrayList<String>();
+	List<String> AP_SUBJECTS = new ArrayList<String>();
+	List<String> SCOPE_CONTENT = new ArrayList<String>();
+	String CUSTODHIST = "";
+	String PHYSDESC = "";
+	
+	
+}
