ef7281a2ddcad75764992c95a12dfe65b307de52
KepaJRodriguez <kepa.rodriguez@gmail.com>
2016-01-12 18:11:18 +0200

    Update of the Application`

--- src/main/java/il/org/yadvashem/alignment_collection_pair/App.java
+++ src/main/java/il/org/yadvashem/alignment_collection_pair/App.java
@@ -1,18 +1,27 @@
 package il.org.yadvashem.alignment_collection_pair;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.xml.stream.XMLStreamException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws XMLStreamException, IOException
     {
         
 
     	String ead_1 = args[0];
     	String ead_2 = args[1];
     	
+    	ArchCollection col_1 = ReadEAD.readEADfile(ead_1);
+    	ArchCollection col_2 = ReadEAD.readEADfile(ead_2);
+    	
+    	PrintTable.printComparisonTable(col_1, col_2);
     	
     }
 }
